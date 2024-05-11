13673191d43762f578bef8115eb428f20418b6bf
Babahasko <zakutni4ek@gmail.com>
2024-05-11 22:29:03 +0300

    Add email authentication and email check

--- hallogramm/hallogramm/settings.py
+++ hallogramm/hallogramm/settings.py
@@ -85,6 +85,11 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
