21b5e96c8424a8b0ecda55527e8c90374483d496
Babahasko <zakutni4ek@gmail.com>
2024-05-14 00:13:08 +0300

    Error connection to database

--- hallogramm/hallogramm/settings.py
+++ hallogramm/hallogramm/settings.py
@@ -84,9 +84,13 @@ WSGI_APPLICATION = 'hallogramm.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "hallogramm_db",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "127.0.0.1",
+        "PORT": "5431",
     }
 }
 
